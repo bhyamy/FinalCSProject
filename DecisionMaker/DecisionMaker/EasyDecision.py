233a0ff593b698d77a14b1581ccbed2946ae3809
DorAm1010 <doramrani1010@gmail.com>
2021-07-23 18:09:52 +0300

    Decision Maker
    Creating decision manager for incoming requests and data, 23/7.

---
+++ DecisionMaker/DecisionMaker/EasyDecision.py
@@ -0,0 +1,6 @@
+from DecisionMaker.DecisionMaker import DecisionMaker
+
+
+class EasyDecision(DecisionMaker):
+    def take_decision(self):
+        pass
---
+++ DecisionMaker/cnfigs.py
@@ -0,0 +1,13 @@
+FORMAT = 'utf-8'
+
+UNITY_BUFFER_SIZE = 1
+UNITY_PORT = 5000
+UNITY_IP = 'localhost'
+UNITY_REQUEST = '1'
+UNITY_DISCONNECT = '0'
+
+EEG_PORT = 8080
+EEG_IP = ''
+EEG_GET_DATA_MSG = bytes('1', encoding='utf8')
+EEG_DISCONNECT_MSG = bytes('0', encoding='utf8')
+EEG_BUFFER_SIZE = 20000
