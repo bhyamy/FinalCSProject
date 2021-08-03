b960b37cdf4c81a533727f69d160310d6045d99f
DorAm1010 <doramrani1010@gmail.com>
2021-08-03 20:29:09 +0300

    FFTProcessor changed process() output to vector of max value from every row of fft except the first.

--- DecisionMaker/Processor/FFTProcessor.py
+++ DecisionMaker/Processor/FFTProcessor.py
@@ -14,7 +14,9 @@ class FFTProcessor(Processor):
         msg = ''
         msg += response.decode(FORMAT)
         data = np.mat(msg)
-        # incoming is 8 (samples) X 71 (channels - 64 EEG + 7 GSR)
+        # incoming is 512 (samples) X 71 (channels - 64 EEG + 7 GSR)
         # take EEG channels i.e. 1-64
         data = np.delete(data, np.s_[64:72], 1)
-        self.processed_data = np.fft.fft2(data)
+        fft = np.fft.fft2(data)
+        
+        self.processed_data = [max(row) for row in fft[1:]]
